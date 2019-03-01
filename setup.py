60da38ecc5f45c2fa15da5ee869d7f6fe6ef7299
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2019-03-01 11:41:43 -0500

    Upgrade version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="bemval",
-    version="0.0.5",
+    version="0.0.6",
     description="Easily validate emails",
     long_description=README,
     long_description_content_type="text/markdown",
